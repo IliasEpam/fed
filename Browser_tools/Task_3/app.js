92704216e0b10d49e82bea1580936e00f28eec7a
IliasEpam <fort-92@mail.ru>
2017-04-05 19:08:14 +0400

    browser tolls first try

---
+++ Browser_tools/Task_3/app.js
@@ -0,0 +1,22 @@
+﻿window.onload = function(){
+	var count = document.getElementById('counter');
+	var countVal = window.history.length;
+	count.innerHTML = 'Pages visited: ' + countVal;
+	
+	var browser = document.getElementById('nav') || 0;
+	if (window.navigator.userAgent.search('Chrome') != -1){
+		browser.innerHTML = navigator.userAgent + '<br>' + navigator.appVersion;
+	}
+	else if (window.navigator.userAgent.search('Mozilla') != -1){
+		browser.innerHTML = navigator.appName + '<br>' + navigator.cookieEnabled;
+	}
+	else if (window.navigator.userAgent.search('IE') != -1){
+		navigator.geolocation.getCurrentPosition(success);
+		function success(position) {
+			var lat = position.coords.latitude;
+			var lon = position.coords.longitude;
+			var browser = document.getElementById('nav') || 0;
+			browser.innerHTML = navigator.platform + '<br>' + lat + '<br>' + lon;
+		}
+	}
+}
